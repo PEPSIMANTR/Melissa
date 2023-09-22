215156feb8c1ec69d08df19bb5b37e63a379ac94
PEPSIMANTR <lenna1327_@outlook.com.tr>
2023-09-23 00:05:24 +0300

    We are so back
    Vargas reminded me about this project, and I decided to restart this project. I rewrote the entire code from scratch and now the current code is actually good, much better than the previous mess,. It's so good that I'll salvage some code from here to Alyssa HTTP Server.
    
    I also reordered the stuff to do. First is completing the base HTTP (which is done already), second is implementing the threading, and third is implementing vectors.

--- Melissa.c
+++ Melissa.c
@@ -1,177 +1,153 @@
+// Melissa HTTP Server Project
+// 
+// This is a HTTP Server that aims to be light and fast, up to a point that running as kernel extension is the target.
+// It'll work as accelerator for a higher level HTTP server, mainly the Alyssa HTTP Server (https://github.com/PEPSIMANTR/AlyssaHTTPServer)
+// Has (will have) some features as well such as range requests and more.
+// 
+// Copyright(C) 2024 Alyssa Software - AfferoGPLv3+ licensed.
+
 #include "Melissa.h"
 #pragma warning(disable:4996)
 
-void ServerHeaders(struct HeaderParameters* h, struct ClientInfo* cl) {
-	memset(cl->SendBuffer, 0, 4096);
-	strcpy(cl->SendBuffer, "HTTP/1.1 "); int pos = 9;
-	switch (h->StatusCode) {
-	case 200: strcpy(&cl->SendBuffer[pos], "200 OK\r\n"); pos += 8; break;
-	case 206: strcpy(&cl->SendBuffer[pos], "206 Partial Content\r\n"); pos += 21; break;
-	case 400: strcpy(&cl->SendBuffer[pos], "400 Bad Request\r\n"); pos += 17; break;
-	case 404: strcpy(&cl->SendBuffer[pos], "404 Not Found\r\n"); pos += 15; break;
-	case 500: strcpy(&cl->SendBuffer[pos], "500 Internal Server Error\r\n"); pos += 27; break;
-	case 501: strcpy(&cl->SendBuffer[pos], "501 Not Implemented\r\n"); pos += 21; break;
-	default: strcpy(&cl->SendBuffer[pos], "501 Not Implemented\r\n"); pos += 21; break;
+char* SendBuffer; char* RecvBuffer;
+
+void ServerHeaders(HeaderParameters* p) {
+	unsigned short off = 9; strcpy(SendBuffer, "HTTP/1.1 ");
+	switch (p->StatusCode) {
+		case 200: strcat(SendBuffer, "200 OK\r\n"); off += 8; break;
+		case 404: strcat(SendBuffer, "404 Not Found\r\n"); off += 15; break;
+		default : strcat(SendBuffer, "501 Not Implemented\r\n"); off += 21; break;
 	}
-	strcat(&cl->SendBuffer[pos], "Content-Length: "); pos += 16;
-	if (h->ContentLength) {
-		sprintf(&cl->SendBuffer[pos], "%lld", h->ContentLength);
-		while (h->ContentLength) {
-			pos++; h->ContentLength /= 10;
+	off += sprintf(&SendBuffer[off], 
+		"Content-Length: %lld\r\n"
+		"Server: Melissa/0.1\r\n", p->ContentLength);
+	if (p->ContentType)
+		off += sprintf(&SendBuffer[off], "Content-Type: %s\r\n", p->ContentType);
+	SendBuffer[off] = '\r'; SendBuffer[off + 1] = '\n'; off += 2;
+	send(p->CliPtr->PollPtr->fd, SendBuffer, off, 0);
+}
+
+void Get(ClientInfo* c) {
+	HeaderParameters p = hpDefault; p.CliPtr = c;
+	FILE* f = fopen(c->ReqPath, "rb");
+	if (f) {// File opened successfully.
+//#ifdef _WIN32
+//		LARGE_INTEGER _fs; int x = GetFileSizeEx(f, &_fs);
+//		if (!x) { x = GetLastError(); }
+//		p.ContentLength = _fs.QuadPart;
+//#endif
+		fseek(f, 0, SEEK_END); p.ContentLength = _ftelli64(f); fseek(f, 0, SEEK_SET);
+		p.ContentType = "text/html";
+		p.StatusCode = 200; ServerHeaders(&p);
+		
+		// Read the file.
+		while (p.ContentLength >= 4096) { 
+			fread(SendBuffer, 4096, 1, f); send(c->PollPtr->fd, SendBuffer, 4096, 0); 
+			p.ContentLength -= 4096;
 		}
+		if (p.ContentLength) {
+			fread(SendBuffer, p.ContentLength, 1, f); send(c->PollPtr->fd, SendBuffer, p.ContentLength, 0);
+		}
+		fclose(f); return;
 	}
 	else {
-		cl->SendBuffer[pos] = '0'; pos++;
+		p.StatusCode = 404; ServerHeaders(&p); return;
 	}
-	strcat(&cl->SendBuffer[pos], "\r\n"); pos += 2;
-	if (h->FileMime) {
-		strcat(&cl->SendBuffer[pos], "Content-Type: "); pos += 14;
-		strcat(&cl->SendBuffer[pos], h->FileMime); pos += strlen(h->FileMime); strcat(&cl->SendBuffer[pos], "\r\n"); pos += 2;
-	}
-	strcat(&cl->SendBuffer[pos], "Server: Melissa/0.1\r\n"); pos += 21; strcat(&cl->SendBuffer[pos], "\r\n"); pos += 2;
-	if (send(cl->s, cl->SendBuffer, pos, 0) < 0)
-		abort();
 }
 
-void Get(struct ClientRequest* cl) {// Open the requested file, for now it'll handle whole request at once as polling is not implemented yet.
-	struct HeaderParameters h = HeaderParametersDefault;
-	//cl->File = fopen(cl->RequestPath, "rb");
-	if (cl->File) {// These will be replaced with system calls rather than stdio
-		h.StatusCode = 200; size_t FileSize = 0;
-		fseek(cl->File, 0, FILE_END); FileSize = ftell(cl->File); fseek(cl->File, 0, 0);
-		h.ContentLength = FileSize; ServerHeaders(&h, cl);
-		while (FileSize) {
-			if (FileSize > 4096) {
-				fread(cl->cl->SendBuffer, 4096, 1, cl->File); FileSize -= 4096;
-				send(cl->cl->s, cl->cl->SendBuffer, 4096, 0);
-			}
-			else {
-				fread(cl->cl->SendBuffer, FileSize, 1, cl->File);
-				send(cl->cl->s, cl->cl->SendBuffer, FileSize, 0); FileSize = 0;
-			}
-		}
-		fclose(cl->File);
+void ParseHeader(ClientInfo* c) {
+	if (WaitForSingleObject(c->Mtx, 100) != WAIT_OBJECT_0) return; // Cannot acquire mutex, return.
+	unsigned short Recv,
+				   pos = 0;//Position of EOL
+	if (c->LastRecvLine) {
+		memcpy(RecvBuffer, c->LastRecvLine, sizeof(c->LastRecvLine));
+		Recv = sizeof(c->LastRecvLine); free(c->LastRecvLine); c->LastRecvLine = NULL;
+		Recv += recv(c->PollPtr->fd, &RecvBuffer[Recv], 4096 - Recv, 0);
 	}
-	else {
-		h.StatusCode = 404; ServerHeaders(&h, cl->cl);
-	}
-	if (cl->CloseConnection) shutdown(cl->cl->s, 2);
-	return;
-}
+	else
+		Recv = recv(c->PollPtr->fd, RecvBuffer, 4096, 0);
+	if (c->Flags ^ 1) {// First line is not parsed yet.
+		for (; pos < Recv; pos++)
+			if (RecvBuffer[pos] < 32) {
+				if (RecvBuffer[pos] > 0) {
+					char _pos = 0;
+					if (!strncmp(RecvBuffer, "GET", 3)) {
+						c->ReqType = 1; _pos = 4;
+					}
+					else if (!strncmp(RecvBuffer, "POST", 4)) {
+						c->ReqType = 2; _pos = 5;
+					}
+					else if (!strncmp(RecvBuffer, "HEAD", 4)) {
+						c->ReqType = 5; _pos = 5;
+					}
+					else {
+						
+					}
+					c->ReqPath = malloc(pos - _pos - 1); memcpy(c->ReqPath, "htroot/", 7); memcpy(&c->ReqPath[7], &RecvBuffer[_pos], pos - _pos - 9); c->ReqPath[pos - _pos - 2] = 0;
+					if (!strncmp(&RecvBuffer[pos - 8], "HTTP/1.", 7)) { 
+						c->Flags |= 1; 
+						if (RecvBuffer[pos - 1] == '0') {// HTTP/1.0 client
+							c->Close = 1;
+						}
+						pos++;
+						if (RecvBuffer[pos] < 31) pos++; // line delimiters are CRLF, iterate pos one more.
+						break;
+					}
+					else {
 
-void ParseHeader(struct ClientInfo* c) {
-	struct ClientRequest cl = ClientRequestDefault; cl.cl = c;
-	int Received = recv(c->s, c->RecvBuffer, 4096, 0);
-	if (Received <= 0) {
-		shutdown(c->s, 2); closesocket(c->s); return;
-	}
-	ClientRequestReset(&cl);
-	unsigned short pos1 = 0, pos2 = 0;
-	while (pos1 < Received) {
-		if (c->RecvBuffer[pos1] > 31) { pos1++; continue; }// Iterate till end of line
-
-		if (cl.RequestPath == 0) {// We are on the first line.
-			unsigned short end = pos1; pos1 = pos2;
-			while (pos2 < end) {
-				if (c->RecvBuffer[pos2] != ' ') { pos2++; continue; }// Iterate till space
-				if (cl.RequestType == 0) {// Request type
-					if (!strncmp(&c->RecvBuffer[pos1], "GET", 3)) cl.RequestType = 1;
-					else cl.RequestType = 2;
-					pos2++;
+					}
 				}
-				else if (cl.RequestPath == 0) {
-					cl.RequestPath = malloc(pos2 + 2 - pos1); memset(cl.RequestPath, 0, pos2 + 2 - pos1);
-					memcpy(&cl.RequestPath[1], &c->RecvBuffer[pos1], pos2 - pos1); cl.RequestPath[0] = '.'; break;
+				else {
+					c->LastRecvLine = malloc(Recv);
+					memcpy(c->LastRecvLine, RecvBuffer, Recv); 
+					goto ParseReturn;
 				}
-				pos1 = pos2;
 			}
-			pos2++;
-			if (!strncmp(&c->RecvBuffer[pos2], "HTTP/", 5)) {
-				pos2 += 5;
-				if (!strncmp(&c->RecvBuffer[pos2], "1.0", 3))
-					cl.CloseConnection = 1;
+	}
+	// Parse the lines
+	for (unsigned short i = pos; i < Recv; i++) {
+		if (RecvBuffer[i] > 31) continue;
+		if (pos - i == 1) {// End of headers
+			switch (c->ReqType) {
+				case 1:
+					Get(c);
+				default:
+					break;
 			}
+			if (c->Close) shutdown(c->PollPtr->fd, 2);
+			ciReset(c); goto ParseReturn;
 		}
-		// We are on later lines (key: value)
-		else if (pos1 - pos2 >= 6) {// Check if line is not shorter than header keys we're comparing for preventing an potential buffer overrun.
-			if (!strncmp(&c->RecvBuffer[pos2], "Host: ", 6)) {
-
-			}
-			else if (!strncmp(&c->RecvBuffer[pos2], "Range: ", 7)) {
-
-			}
+		else if (!strncmp(&RecvBuffer[pos], "Connection", 10)) {
+			if (!strcmp(&RecvBuffer[pos + 12], "close")) c->Close = 1;
+			else c->Close = 0;
 		}
-		else if (pos1 - pos2 == 0) {// We reached the empty line, which means end of headers.
-			switch (cl.RequestType) {
-			case 1:
-				Get(&cl);
-			case 5:
-			default:
-				break;
-			}
-			return;
+		else if (!strncmp(&RecvBuffer[pos], "Host", 4)) {// Headers will be parsed that way, you got the point. + offsets also includes the ": ".
+			//memcpy(&RecvBuffer[pos + 6, c->Host, pos - pos - 6]);
+#ifdef _DEBUG
+			printf("asdasd");
+#endif
 		}
-		pos1++; if (&c->RecvBuffer[pos1] < 32) pos1++;//Check if line delimiters are CRLF or LF
-		pos2 = pos1;
+		pos = i+1;
+		if (RecvBuffer[pos] < 31) pos++; // line delimiters are CRLF, iterate pos one more.
 	}
-	return;
-}
-
-void ThreadDriver(LPVOID lpParam) {
-	//WinAPI is fucking broken. It somehow overwrites the parameter of this func, so i'll copy it.
-	//Update: fopen on Get() makes it change. What the fuck Microsoft???
-	struct ThreadInfo* t = lpParam;
-	//memcpy(t, lpParam, sizeof(struct ThreadParameters));
-	struct ClientInfo* ClientPtr = NULL;
-	while (read(t->RecvPipe, &ClientPtr, sizeof(ClientPtr))) {
-		//memset(t->OccupicationFlag, 0xFF, 1);
-		ParseHeader(ClientPtr);
-		printf("%d", t->ThreadID);
-		//ClientPtr->PollPtr->events = POLLIN;
-		//memset(t->OccupicationFlag, 0, 1);
+	// All complete lines are parsed, check if there's a incomplete remainder
+	if (pos < Recv) {
+		c->LastRecvLine = malloc(Recv - pos); memcpy(c->LastRecvLine, RecvBuffer[pos], Recv - pos);
 	}
-	switch (errno)
-	{
-	case EBADF:
-		perror("Bad file descriptor!");
-		break;
-	case ENOSPC:
-		perror("No space left on device!");
-		break;
-	case EINVAL:
-		perror("Invalid parameter: buffer was NULL!");
-		break;
-	default:
-		// An unrelated error occurred
-		perror("Unexpected error!");
-	}
-	abort();
-}
-
-void ThreadDriver2(LPVOID lpParam) {
-	//WinAPI is fucking broken. It somehow overwrites the parameter of this func, so i'll copy it.
-	struct ThreadParameters* t = malloc(sizeof(struct ThreadParameters));
-	memcpy(t, lpParam, sizeof(struct ThreadParameters));
-	struct ClientInfo* ClientPtr = NULL;
-	while (read(t->ti->RecvPipe, &ClientPtr, sizeof(ClientPtr))) {
-		//memset(t->OccupicationFlag, 0xFF, 1);
-		ParseHeader(ClientPtr);
-		printf("%d", t->ti->ThreadID);
-		//ClientPtr->PollPtr->events = POLLIN;
-		//memset(t->OccupicationFlag, 0, 1);
-	}
-	abort();
+ParseReturn:
+	memset(RecvBuffer, 0, Recv); ReleaseMutex(c->Mtx); return;
 }
 
 int main() {
+	SendBuffer = malloc(4096); RecvBuffer = malloc(4096);
+	memset(SendBuffer, 0, 4096); memset(RecvBuffer, 0, 4096);
 #ifdef _WIN32
-	// Initialze winsock
-	WSADATA wsData; WORD ver = MAKEWORD(2, 2);
-	if (WSAStartup(ver, &wsData)) {
+	// Initialize WinSock
+	WSADATA _WSADATA; WORD _wsver = MAKEWORD(2, 2);
+	if (WSAStartup(_wsver, &_WSADATA)) {
 		printf("Can't Initialize winsock! Quitting\n"); return -1;
 	}
 #endif
-
 	// Create a socket
 	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
 	if (listening == INVALID_SOCKET) {
@@ -189,14 +165,17 @@ int main() {
 	socklen_t len = sizeof(hint);
 	bind(listening, (struct sockaddr*)&hint, sizeof(hint));
 	if (getsockname(listening, (struct sockaddr*)&hint, &len) == -1) {//Cannot reserve socket
-		//std::cout << "Error binding socket on port " << port[i] << std::endl << "Make sure port is not in use by another program."; 
+		printf("Error binding socket on port %d\n Make sure port is not in use by another program.\n", 8000);
 		return -2;
 	}
 	//Linux can assign socket to different port than desired when is a small port number (or at leats that's what happening for me)
-	else if (8000 != ntohs(hint.sin_port)) { /*std::cout << "Error binding socket on port " << port[i] << " (OS assigned socket on another port)" << std::endl << "Make sure port is not in use by another program, or you have permissions for listening that port." << std::endl;*/ return -2; }
+	else if (8000 != ntohs(hint.sin_port)) { 
+		printf("Error binding socket on port %d (OS assigned socket on another port)\n Make sure port is not in use by another program, or you have permissions for listening that port.\n", 8000);
+		return -2; 
+	}
 	listen(listening, SOMAXCONN);
 
-	// Listen the socket and accept the connection
+	// Client stuff
 	struct sockaddr_in client; char host[NI_MAXHOST] = { 0 }; // Client's IP address
 #ifndef _WIN32
 	unsigned int clientSize = sizeof(client);
@@ -205,115 +184,55 @@ int main() {
 #endif
 	// Setup the structures
 	inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-	struct Vector PollVec = VectorDefault; InitializeVector(&PollVec, sizeof(struct pollfd));
-	struct Vector SockType = VectorDefault; InitializeVector(&SockType, 1);
-	struct Vector ClientVec = VectorDefault; InitializeVector(&ClientVec, sizeof(struct ClientInfo*));
-	struct Vector ThreadVec = VectorDefault; InitializeVector(&ThreadVec, sizeof(struct ThreadInfo*));
-	//struct Vector ThreadPrVec = VectorDefault; InitializeVector(&ThreadPrVec, sizeof(struct ThreadParameters));
-	//struct Vector ThreadHandleVec = VectorDefault; InitializeVector(&ThreadHandleVec, sizeof(HANDLE));
-	struct Vector ThreadOccupancyVec = VectorDefault; InitializeVector(&ThreadOccupancyVec, 1);
-	for (int i = 0; i < 1; i++) {
-		struct ThreadInfo* t = malloc(sizeof(ThreadInfoDefault)); char zero = 0; int pipes[2] = { 0 }; struct ThreadParameters tp = ThreadParametersDefault;
-		_pipe(&pipes, sizeof(void*), 0);
-		t->RecvPipe = pipes[0]; t->SendPipe = pipes[1]; t->ThreadID = i + 1;
-		//AddElement(&ThreadVec, &t, sizeof(t));
-		AddElement(&ThreadOccupancyVec, &zero, 1);
-		//tp.OccupicationFlag = &ThreadOccupancyVec.Data[i]; tp.ti = GetElement(&ThreadVec, i); AddElement(&ThreadPrVec, &tp, sizeof(tp));
-		AddElement(&ThreadVec, &t, 8);
-		if (!CreateThread(NULL, 0, ThreadDriver, t, 0, NULL)) abort();
-	}
+
 	struct pollfd EmptyPollfd = { .fd = DummySock,.events = 0 };
-#ifdef _WIN32
-	HANDLE VecMutex = CreateMutex(NULL, 0, NULL);
-#endif
 	char SockTypeElement = 0; struct pollfd PollfdElement = EmptyPollfd;
 	SockTypeElement = 1; PollfdElement.fd = listening; PollfdElement.events = POLLIN;
-	AddElement(&SockType, &SockTypeElement, 1); AddElement(&PollVec, &PollfdElement, sizeof(struct pollfd)); 
-	AddElement(&ClientVec, &NullPtr, sizeof(NullPtr));
-	int Connections = 0, EmptyElements = 0; struct pollfd* PollPointer;
-	//goto xy;
-	while ((Connections = WSAPoll(PollVec.Data, PollVec.MaxSize, -1)) > 0) {
-		EmptyElements = 0;
-		for (size_t i = 0; i < PollVec.MaxSize; i++) {
-			if (PollVec.Occupied[i] == 0) { EmptyElements++; continue; }
-			//PollPointer = &PollVec.Data[i * sizeof(struct pollfd)];
-			PollPointer = GetElement(&PollVec, i - EmptyElements);
-			if (PollPointer->revents & POLLIN) {
-				if (SockType.Data[i] & 1) {// Listening socket
-					SOCKET client = accept(listening, NULL, NULL);
-					PollfdElement.fd = client;
-					AddElement(&PollVec, &PollfdElement, sizeof(PollfdElement));
-					SockTypeElement = 0; AddElement(&SockType, &SockTypeElement, 1);
-					struct ClientInfo* cl = malloc(sizeof(struct ClientInfo)); ClientInfoInit(cl); cl->s = client; cl->PollPtr = PollPointer;
-					AddElement(&ClientVec, &cl, sizeof(cl));
+
+	// Start polling
+	struct pollfd PollArray[256]; unsigned short PollCnt = 1;
+	ClientInfo* ciArray[256] = { NULL };
+	PollArray[0] = (struct pollfd){ .fd = listening, .events = POLLIN };
+	unsigned short Connections;
+
+	while ((Connections=WSAPoll(PollArray, PollCnt, -1))>0) {
+		if (PollArray[0].revents & POLLIN) {// Incoming connection, accept.
+			Connections--; SOCKET ClientSock = accept(listening, (struct sockaddr*)&client.sin_addr, &clientSize);
+			// Search for freed space in array
+			for (unsigned short i = 1; i < PollCnt; i++) {
+				if (PollArray[i].fd = DummySock) {
+					PollArray[i] = (struct pollfd){
+						.fd = ClientSock, .events = POLLIN
+					};
+					ciArray[i] = malloc(sizeof(ClientInfo)); ciInit(ciArray[i]);
+					ciArray[i]->PollPtr = &PollArray[i]; ciArray[i]->Mtx = CreateMutex(NULL, FALSE, NULL);
+					ParseHeader(ciArray[i]); goto IncomingAdded;
 				}
-				else {// Client socket
-					/*struct ClientInfo cl = ClientInfoDefault;
-					memcpy(&cl, GetElement(&ClientVec, i-EmptyElements), sizeof(cl));
-					ParseHeader(cl);*/
-
-					// Disable polling for that socket, subthreads will re-enable it when request is done.
-					//PollPointer->events = 0;
-
-					// Check for any free threads, pass the client to that thread.
-					while (1) {
-						for (char j = 0; j < 1; j++) {
-							if (!ThreadOccupancyVec.Data[j]) {
-								struct ThreadInfo* ThreadPtr = 0;
-								memcpy(&ThreadPtr, &ThreadVec.Data[j], 8);
-								if (_write(ThreadPtr->SendPipe, GetElement(&ClientVec, i - EmptyElements), sizeof(void*)) < 0)
-									abort();
-
-								goto PassedToThread;
-							}
-						}
-						Sleep(1);
-					}
-				PassedToThread:;
+			}
+			// If none, emplace back
+			PollArray[PollCnt] = (struct pollfd){
+				.fd = ClientSock, .events = POLLIN
+			}; 
+			ciArray[PollCnt] = malloc(sizeof(ClientInfo)); ciInit(ciArray[PollCnt]);
+			ciArray[PollCnt]->PollPtr = &PollArray[PollCnt]; ciArray[PollCnt]->Mtx = CreateMutex(NULL, FALSE, NULL);
+			// Handle that client too
+			ParseHeader(ciArray[PollCnt]);
+			PollCnt++;
+		}
+	IncomingAdded:
+		for (unsigned short i = 1; Connections; i++) {
+			if (PollArray[i].revents & POLLIN) {// Incoming data from client
+				ParseHeader(ciArray[i]);
+				Connections--;
+			}
+			else if (PollArray[i].revents & (POLLERR | POLLHUP)) { // Connection lost to client.
+				if (WaitForSingleObject(ciArray[i]->Mtx, 0) == WAIT_OBJECT_0) {// Mutex acquired.
+					closesocket(PollArray[i].fd); PollArray[i] = EmptyPollfd; 
+					ReleaseMutex(ciArray[i]->Mtx); CloseHandle(ciArray[i]->Mtx);
+					free(ciArray[i]); ciArray[i] = NULL;
 				}
 				Connections--;
 			}
-			else if (PollPointer->revents & POLLHUP) {// Connection terminated.
-				struct ClientInfo* cl = NULL; memcpy(&cl, GetElement(&ClientVec, i - EmptyElements), 8);
-				ClientInfoCleanup(cl); free(cl);
-				SockType.Data[i] = 0;
-				memcpy(PollPointer, &EmptyPollfd, sizeof(EmptyPollfd)); Connections--; DeleteElement(&PollVec, i - EmptyElements);
-				DeleteElement(&ClientVec, i - EmptyElements); DeleteElement(&SockType, i - EmptyElements); EmptyElements++;
-			}
-			if (!Connections) break;
 		}
 	}
-	wchar_t* s = NULL; int errnum = WSAGetLastError();
-	FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL, errnum,
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPWSTR)&s, 0, NULL);
-	fprintf(stderr, "%S\n", s);
-	abort();
-	/*while (1) {
-		SOCKET ClientSock = accept(listening, (struct sockaddr*)&client, &clientSize);
-		if (ClientSock == SOCKET_ERROR) {
-			printf("accept() failure.\n"); return -3;
-		}
-		struct ClientInfo cl = ClientInfoDefault; cl.s = ClientSock;
-		cl.RecvBuffer = malloc(4096); cl.SendBuffer = malloc(4096);
-		memset(cl.RecvBuffer, 0, 4095); memset(cl.SendBuffer, 0, 4096);
-		ParseHeader(cl);
-	}*/
-	// Vector testing code
-xy:
-	struct Vector v = VectorDefault;
-	InitializeVector(&v, sizeof(int));
-	int x = 0x7aaaaaaa;
-	for (int i = 0; i < 5; i++) {
-		AddElement(&v, &x, sizeof(int)); x++;
-	}
-	x = 0;
-	for (size_t i = 0; i < 5; i++) {
-		memcpy(&x, GetElement(&v, i), 4);
-	}
-	DeleteElement(&v, 2);
-	AddElement(&v, &x, sizeof(int));
-
-	return 0;
 }
--- Melissa.h
+++ Melissa.h
@@ -1,138 +1,43 @@
 #pragma once
-#ifndef MelissaHeader
-#define MelissaHeader
+// Inclusions
 #include <stdio.h>
 #include <string.h>
 #ifdef _WIN32
 	#include <WS2tcpip.h>
-	#pragma comment (lib, "ws2_32.lib")
+	#pragma comment(lib,"WS2_32.lib")
 	#include <io.h>
-#endif // _WIN32
-
-extern SOCKET DummySock;
-
-typedef struct ClientInfo;
-struct ClientInfo {
-	SOCKET s;
-	char* SendBuffer; char* RecvBuffer;
-	struct pollfd* PollPtr;
-};
-struct ClientInfo ClientInfoDefault = {
-	.RecvBuffer = NULL, .SendBuffer = NULL, .s = 0, .PollPtr = NULL
-};
-struct ClientRequest {
-	struct ClientInfo* cl;
-	char* RequestPath; unsigned char RequestType;
-	unsigned char CloseConnection;// There's no bool in C lol
-	char* Host;
-	unsigned long long RangeStart; unsigned long long RangeEnd;
-	FILE* File;
-};
-struct ClientRequest ClientRequestDefault = {
-	.CloseConnection = 0, .RequestPath = 0, .RequestType = 0, .RangeStart = 0, .RangeEnd = 0, .Host = 0, .File = 0
-};
-static inline void ClientInfoInit(struct ClientInfo* cl) {
-	cl->RecvBuffer = malloc(4096); cl->SendBuffer = malloc(4096);
-	memset(cl->RecvBuffer, 0, 4096); memset(cl->SendBuffer, 0, 4096);
-}
-static inline void ClientRequestReset(struct ClientRequest* cl) {
-	if (cl->RequestPath) free(cl->RequestPath);
-	if (cl->Host) free(cl->Host);
-	//memset(cl->cl->RecvBuffer, 0, 4096); memset(cl->cl->SendBuffer, 0, 4096);
-}
-static inline void ClientInfoCleanup(struct ClientInfo* cl) {
-	free(cl->SendBuffer); free(cl->RecvBuffer); closesocket(cl->s);
-}
-
-struct ThreadInfo {
-	int SendPipe, RecvPipe, ThreadID;//SendPipe is the pipe main thread writes to, RecvPipe is the pipe threads read from.
-};
-struct ThreadInfo ThreadInfoDefault = {
-	.SendPipe = 0, .RecvPipe = 0,.ThreadID = 0
-};
-struct ThreadParameters {
-	struct ThreadInfo* ti; char* OccupicationFlag;
-};
-struct ThreadParameters ThreadParametersDefault = {
-	.ti = 0, .OccupicationFlag = 0
-};
-
-typedef struct HeaderParameters;
-struct HeaderParameters {
-	unsigned short StatusCode;
-	size_t ContentLength; char* FileMime;
-};
-struct HeaderParameters HeaderParametersDefault = {
-	.ContentLength = 0, .FileMime=0, .StatusCode=0
-};
-static inline void HeaderParametersReset(struct HeaderParameters* h) {
-	if (h->FileMime) free(h->FileMime);
-}
-
-/// <summary>
-/// Implementation of C++ Vectors in C
-/// NOT suitable for using on other code as it's specifically written for Melissa and violates some vector rules.
-/// </summary>
-typedef struct Vector;
-struct Vector {
-	unsigned char* Data; // Actual data
-	unsigned char* Occupied; // Array for indicating which offsets are allocated with data or unused, 0xFF if used.
-	int CurrentSize; int MaxSize; unsigned int ElementSz;
-};
-struct Vector VectorDefault = {
-	.Data = 0, .Occupied=0, .CurrentSize = 0, .MaxSize = 0, .ElementSz = 0
-};
-static inline void InitializeVector(struct Vector* Vec, int ElementSz) {
-	Vec->ElementSz = ElementSz; Vec->Data = malloc(ElementSz); 
-	Vec->Occupied = malloc(1); Vec->Occupied[0] = 0;
-	Vec->MaxSize = 1; return;
-}
-static inline void* AddElement(struct Vector* Vec, void* Element, int ElementSz) {
-	if (ElementSz != Vec->ElementSz) abort(); //throw("AddElement(): Added element is not equal to vector element size.\n");
-	// Search for an empty space first and reuse it
-	for (size_t off = 0; off < Vec->MaxSize; off++) {
-		if (Vec->Occupied[off] != 0xFF) {// If this offset is unused(free)
-			memcpy(&Vec->Data[off * Vec->ElementSz], Element, ElementSz);
-			Vec->CurrentSize++;  Vec->Occupied[off] = 0xFF;
-			return &Vec->Data[off * Vec->ElementSz];
-		}
-	}
-	// If not found, extend the vector and add to end.
-	Vec->Data = realloc(Vec->Data, (Vec->MaxSize + 1) * ElementSz);  Vec->Occupied = realloc(Vec->Occupied, Vec->MaxSize + 1);
-	if (!Vec->Data || !Vec->Occupied) abort();
-	memcpy(&Vec->Data[(Vec->MaxSize)*ElementSz], Element, ElementSz); Vec->Occupied[Vec->MaxSize] = 0xFF;
-	Vec->MaxSize++; Vec->CurrentSize++; return &Vec->Data[Vec->MaxSize-ElementSz];
-}
-static inline void* GetElement(struct Vector* Vec, int Offset) {
-	int DbgOff = Offset;
-	if (Offset >= Vec->CurrentSize) abort(); //throw("GetElement(): Offset is bigger than current element count.\n");
-	for (size_t i = 0; i < Vec->MaxSize; i++) {
-		if (Vec->Occupied[i] == 0xFF) {
-			if (!Offset) {
-				return &Vec->Data[i*Vec->ElementSz];
-			}
-			Offset--;
-		}
-	}
-	abort();
-}
-static inline void DeleteElement(struct Vector* Vec, int Offset) {
-	if (Offset > Vec->CurrentSize) abort(); //throw("GetElement(): Offset is bigger than current element count.\n");
-	for (size_t i = 0; i < Vec->MaxSize; i++) {
-		if (Vec->Occupied[i] == 0xFF) {
-			if (!Offset) {
-				Vec->Occupied[i] = 0; Vec->CurrentSize--; return;
-			}
-			Offset--;
-		}
-	}
-	abort();
-}
-
-void* NullPtr = NULL;
-
-
-
-
-
+#else
+	
 #endif
+// Cross platform
+
+// Structures
+typedef struct {
+	struct pollfd* PollPtr;
+	char* ReqPath;
+	unsigned char ReqType;
+	char* LastRecvLine; // Last received incomplete line piece. 
+	char Flags;
+	char Close;
+#ifdef _WIN32
+	HANDLE Mtx;
+#endif
+} ClientInfo;
+ClientInfo ciDefault = {
+	.Close = 0, .Flags = 0, .ReqType = 0,
+};
+#define ciReset(name) name->Close=0; name->Flags=0; name->ReqType=0; free(name->ReqPath); name->ReqPath=NULL
+#define ciInit(name) name->ReqPath=NULL; ciReset(name); name->LastRecvLine=NULL
+
+typedef struct {
+	size_t ContentLength;
+	unsigned short StatusCode;
+	char* ContentType;
+	ClientInfo* CliPtr;
+} HeaderParameters ;
+HeaderParameters hpDefault = {
+	.ContentLength = 0, .StatusCode = 404, .ContentType = 0, .CliPtr = 0
+};
+// Variables & constants
+
+// Functions
